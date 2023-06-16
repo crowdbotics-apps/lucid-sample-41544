5f795942562a89aab83ffd801c21450564ea5554
Crowdbotics <team@crowdbotics.com>
2023-06-16 19:53:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucid-sample-41544",
+  url: "https://lucid-sample-41544.botics.co",
+  api: "https://lucid-sample-41544.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
